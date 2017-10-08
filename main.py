473b9dacd31299d3f4b2914faa217f19ff70be38
Fernando de Simas <fernando.simas88@gmail.com>
2017-10-08 19:35:51 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,51 @@
+# Clima Itajai 
+# Fernando de Simas - 2017
+# 
+# Busca informações do openWeather e posta na conta twitter.com/climaitajai
+
+import json
+import codecs
+import datetime
+import twitter
+from urllib.request import urlopen
+
+WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?id={}&lang=pt&APPID={}&units=metric'
+TWEET_STR = 'Em {} faz {} graus. Nebulosidade: {} %. Umidade relativa do ar: {} %. [{}] #itajai #climaitajai'
+
+def carrega_config():
+	return json.loads(open('config.json').read())	
+
+def retorna_info_tempo(openweather_key):
+	f = urlopen(WEATHER_URL.format(openweather_key))		
+	reader = codecs.getreader("utf-8")
+	return json.load(reader(f))
+
+def tweet(config, info_tempo):		
+	api = twitter.Api(config['consumer_key'],
+                      config['consumer_secret'],
+                      config['access_token_key'],
+                      config['access_token_secret'])
+	status = TWEET_STR.format(
+			info_tempo['name'],
+			info_tempo['main'].get('temp'), 
+			info_tempo['clouds'].get('all'),
+			info_tempo['main'].get('humidity'),
+			datetime.datetime.now().strftime("%d-%m-%Y %H:%M"))
+	print("Enviando tweet: {}".format(status))
+	api.PostUpdate(status)
+
+def main():
+	config = carrega_config()
+	info_tempo = retorna_info_tempo(config['CIDADE_ID'],config["OPENWEATHER_KEY"])
+	try:
+		tweet(config, info_tempo)			
+	except Exception as e:
+		print("Erro ao enviar tweet: " + str(e))
+	else:
+		pass
+	finally:
+		pass
+	
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Clima Itajaí
+
+Script simples para envio de informações do tempo para a conta do twitter @climaitajai.
+
+Configurando
+
+No arquivo config.json são informados o ID da cidade usada pelo open wheater bem como a chave da API. Também são informados os tokens e keys utilizadas para conexão com Twitter.
+
+`{`  
+  "consumer_key": "",
+  "consumer_secret" :"",
+  "access_token_key": "",
+  "access_token_secret": "",  
+  "OPENWEATHER_KEY": "",  
+  "CIDADE_ID": "3460845"
+`}`
+
+Biblioteca utilizada
+
+Python Twitter https://github.com/bear/python-twitter
